3eb7b83a3e4619645d04f5bde1e13b160a3d1b2b
ksun9435 <87787907+ksun9435@users.noreply.github.com>
2024-06-12 19:04:53 -0400

    youtube??

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World </h1>
+      <iframe
+        src='https://www.youtube.com/embed/E7wJTI-1dvQ'
+        frameborder='0'
+        allow='autoplay; encrypted-media'
+        allowfullscreen
+        title='video'
+      />
     </div>
+    
   );
 }
 
 export default App;
+
